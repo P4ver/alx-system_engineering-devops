0cdf3016f8be162d0e65d91d2c7a72e04588f67f
P4ver <s.pales20@gmail.com>
2024-01-03 07:38:58 -0800

    tsj

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-#must match a 10 digit phone numbr,
+# must match a 10 digit phone numbr,
 # Ruby scrpt that accepts 1 arg and pass it to a regex,
-puts ARGV[0].scan(/^\d{10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join
