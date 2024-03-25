0e7d323f2481df52a2a392cf5d224b498d3a2173
P4ver <s.pales20@gmail.com>
2024-03-25 12:36:47 -0700

    edt

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,17 +8,23 @@ if __name__ == '__main__':
     user = sys.argv[1]
     url_us = 'https://jsonplaceholder.typicode.com/users/' + user
     res_lt = requests.get(url_us)
-    """ANYTHING"""
-    usr_nm = res_lt.json().get('username')
+    usr_nm = res_lt.json().get('username')  # Retrieve username from the response
     task = url_us + '/todos'
     res_lt = requests.get(task)
     tsks = res_lt.json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
+    with open('{}.csv'.format(user), 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        # Write CSV header
+        csv_writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+        
+        # Count the number of tasks
+        task_count = 0
+        
         for task in tsks:
             cmpltd = task.get('completed')
-            """Complete"""
             titltsk = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, usr_nm, cmpltd, titltsk))
+            
+            # Write task details to CSV
+            csv_writer.writerow([user, usr_nm, cmpltd, titltsk])
+            task_count += 1
