29ae0ddd23ac0ba46c38512075c9b364ff9396be
P4ver <s.pales20@gmail.com>
2024-03-25 12:44:30 -0700

    edt

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-""" to CSV"""
+""" to CSV format."""
 import csv
 import requests
 import sys
 
-if __name__ == '__main__':
-    user = sys.argv[1]
-    url_us = 'https://jsonplaceholder.typicode.com/users/' + user
-    res_lt = requests.get(url_us)
-    """ANYTHING"""
-    usr_nm = res_lt.json().get('username')
-    task = url_us + '/todos'
-    res_lt = requests.get(task)
-    tsks = res_lt.json()
+if __name__ == "__main__":
+    usrid = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(usrid)).json()
+    usrnm = user.get("username")
+    tds = requests.get(url + "todos", params={"userId": usrid}).json()
 
-    with open('{}.csv'.format(user), 'w') as csvfile:
-        for task in tsks:
-            cmpltd = task.get('completed')
-            """Complete"""
-            titltsk = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user, usr_nm, cmpltd, titltsk))
+    with open("{}.csv".format(usrid), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [usrid, usrnm, t.get("completed"), t.get("title")]
+         ) for t in tds]
