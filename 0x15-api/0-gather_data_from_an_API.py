53b0e2973035cb1b9a19d7013a066def0ab568c6
P4ver <s.pales20@gmail.com>
2024-03-25 12:19:43 -0700

    tsk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 '''gather employee dt from api.'''
+
+
+import re
 import requests
 import sys
 
+REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-    REST_API = "https://jsonplaceholder.typicode.com"
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            reqst = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            tskRq = requests.get('{}/todos'.format(REST_API)).json()
+            empName = reqst.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, tskRq))
+            compleTsks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    empName,
+                    len(compleTsks),
+                    len(tasks)
+                )
+            )
+            if len(compleTsks) > 0:
+                for task in compleTsks:
+                    print('\t {}'.format(task.get('title')))
