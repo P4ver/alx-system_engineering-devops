0823065f1ec1cb21f31e53439baa21055b92929f
P4ver <s.pales20@gmail.com>
2024-04-09 11:24:25 +0100

    tst

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""module contains function to query subscribers"""
+'''module contains function to query subscribers,'''
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Return the num of subscribers,"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Chrome"
-    }
-    resp = requests.get(url, headers=headers, allow_redirects=False)
-    if resp.status_code == 404:
+    '''Return the num of subscribers,'''
+    user = {'User-Agent': 'Chrome/98.0.4758.102'}
+    resp = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        res = resp.get('data')
+        return res.get('subscribers')
+    except Exception:
         return 0
-    res = resp.json().get("data")
-    return res.get("subscribers")
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
