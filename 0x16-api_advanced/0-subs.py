66193b958b54df58b0d22279f25124ecf43b9a48
P4ver <s.pales20@gmail.com>
2024-04-09 11:25:57 +0100

    tst

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     '''Return the num of subscribers,'''
     user = {'User-Agent': 'Chrome/98.0.4758.102'}
     resp = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
+                        .format(subreddit), headers=user).json()
     try:
         res = resp.get('data')
         return res.get('subscribers')
