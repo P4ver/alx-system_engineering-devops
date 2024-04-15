5a9a022addb5c7610a87ed552902a9133d3f0048
P4ver <s.pales20@gmail.com>
2024-04-15 14:01:44 -0700

    test

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec { 'hard_file_descriptors':
+        provider => shell,
+        command  => 'sed -i "/^holberton hard nofile/s/5/50/g" /etc/security/limits.conf',
+}
+exec { 'soft_file_descriptors':
+        provider => shell,
+        command  => 'sed -i "/^holberton soft nofile/s/4/40/g" /etc/security/limits.conf',
+}
